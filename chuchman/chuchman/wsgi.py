17a989f530e94107346645205633292c98903b0c
stalker0321 <n.grishin04@gmail.com>
2021-03-03 01:08:18 +0200

    Update wsgi.py

--- chuchman/chuchman/wsgi.py
+++ chuchman/chuchman/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chuchman.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chuchman.settings.dev')
 
 application = get_wsgi_application()
