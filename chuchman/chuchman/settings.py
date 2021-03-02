05281d0881b7bf460f0964f235db180faf4eb686
stalker0321 <n.grishin04@gmail.com>
2021-03-02 23:19:44 +0200

    Update settings.py

--- chuchman/chuchman/settings.py
+++ chuchman/chuchman/settings.py
@@ -117,7 +117,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
@@ -127,7 +126,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 TEMPLATES_URL = '/templates/'
 
